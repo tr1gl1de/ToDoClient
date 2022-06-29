7a75d150d1f069aeb345f793b6088b1f2fd6b530
Max <pogatop@yandex.ru>
2022-06-29 16:50:26 +0300

    Add token storage service

---
+++ src/app/_services/token-storage.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+
+const TOKEN_KEY = "jwt";
+const REFRESH_TOKEN_KEY = "refreshToken";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenStorageService {
+
+  constructor() {  }
+
+  public saveToken(token: string): void {
+    localStorage.removeItem(TOKEN_KEY);
+    localStorage.setItem(TOKEN_KEY, token);
+  }
+
+  public getToken(): string | null {
+    return localStorage.getItem(TOKEN_KEY);
+  }
+
+  public saveRefreshToken(refreshToken: string): void {
+    localStorage.removeItem(REFRESH_TOKEN_KEY);
+    localStorage.setItem(REFRESH_TOKEN_KEY, refreshToken);
+  }
+
+  public getRefreshToken(): string | null {
+    return localStorage.getItem(REFRESH_TOKEN_KEY);
+  }
+
+  signOut(): void {
+    localStorage.clear();
+  }
+}
